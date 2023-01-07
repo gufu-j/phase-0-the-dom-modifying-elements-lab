a86407a57c34fbdf4b49d49369d16f2ef479527d
diego <guatemaladiegojose@gmail.com>
2023-01-07 14:59:51 -0500

    commit tree 4e354193f477194f95180aa2104599c576be0198

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const element =  document.getElementById("main");
+element.remove();
+
+
+const newHeader = document.createElement("h1");
+newHeader.id= 'victory'
+newHeader.innerHTML = "YOUR - NAME is the champion";
